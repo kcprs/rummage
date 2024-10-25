7055351ffcf31d6232c3a2aa08665c4af6a5c92a
Kacper Sagnowski <ksagnowski@gmail.com>
2024-10-25 23:25:49 +0200

    Index into an array

--- breaks.py
+++ breaks.py
@@ -7,3 +7,10 @@ def break_main(frame, bp_loc, extra_args, internal_dict):
 
     # Returning False tells lldb not to stop at the breakpoint
     return False
+
+
+def index_int_array(frame, bp_loc, extra_args, internal_dict):
+    array = frame.FindVariable("array")
+    el_3 = array.GetChildAtIndex(3)
+
+    GlobalFileWriter.instance().write("output.log", el_3, loc=Location(frame))
--- main.c
+++ main.c
@@ -3,7 +3,7 @@ int main(int argc, char** argv) {
     int some_value = 1;
     float array[] = {1.f, 1.f, 2.f, 3.f};  // @loupe: break_main
     int a = some_value + argc;
-    some_value = 2;
+    some_value = 2; // @loupe: index_int_array
 
     return 0;  // @loupe: break_main
 }
