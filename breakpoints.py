c35f6778f021bbf7f55190b8e28dcb4eadb9ee99
Kacper Sagnowski <ksagnowski@gmail.com>
2024-10-26 00:07:06 +0200

    Access children of structs

--- breakpoints.py
+++ breakpoints.py
@@ -3,14 +3,29 @@ from loupe import GlobalFileWriter, Location
 
 def break_main(frame, bp_loc, extra_args, internal_dict):
     var = frame.FindVariable("some_value")
+    assert var.IsValid()
+
     GlobalFileWriter.instance().write("output.log", var, loc=Location(frame))
 
-    # Returning False tells lldb not to stop at the breakpoint
-    return False
+    return False # Returning False tells lldb not to stop at the breakpoint
 
 
 def index_int_array(frame, bp_loc, extra_args, internal_dict):
     array = frame.FindVariable("array")
     el_3 = array.GetChildAtIndex(3)
+    assert el_3.IsValid()
 
     GlobalFileWriter.instance().write("output.log", el_3, loc=Location(frame))
+
+    return False
+
+
+def struct_children(frame, bp_loc, extra_args, internal_dict):
+    struct = frame.FindVariable("some_struct")
+    child = struct.GetChildMemberWithName("avg_blorp")
+    assert child.IsValid()
+
+    GlobalFileWriter.instance().write("output.log", struct, loc=Location(frame))
+    GlobalFileWriter.instance().write("output.log", child, loc=Location(frame))
+
+    return False
--- main.c
+++ main.c
@@ -1,9 +1,20 @@
+typedef struct {
+    int num_blorps;
+    float avg_blorp;
+    const char* just_some_chars;
+} SomeStruct;
+
 int main(int argc, char** argv) {
     char* its_an_arg = argv[0];
     int some_value = 1;
     float array[] = {1.f, 1.f, 2.f, 3.f};  // @loupe: break_main
     int a = some_value + argc;
-    some_value = 2; // @loupe: index_int_array
+    some_value = 2;  // @loupe: index_int_array
+
+    SomeStruct some_struct = {.num_blorps = 56,
+                              .avg_blorp = 3.14f,
+                              .just_some_chars = "look, it's a string!"};
+    (void)0;  // @loupe: struct_children
 
     return 0;  // @loupe: break_main
 }
